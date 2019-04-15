6039ef82a80e9a119327eadeb203f37c8b9c02ab
MrXsite <49187820+MrXsite@users.noreply.github.com>
2019-04-15 09:52:06 +0700

    Add files via upload

---
+++ cctv.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+#///////////////////////////////////////////////////////////////////////////////
+#///////////////////////////////////////////////////////////////////////////////
+#////                       _            _  __                              ////
+#////                      | |          (_)/ _|                             ////
+#////                   ___| |_   _  ___ _| |_ ___ _ __                     ////
+#////                  |_  / | | | |/ __| |  _/ _ \ '__|                    ////
+#////                   / /| | |_| | (__| | ||  __/ |                       ////
+#////                  /___|_|\__,_|\___|_|_| \___|_|                       ////
+#////                                                                       ////
+#///////////////////////////////////////////////////////////////////////////////
+#///////////////////////////////////////////////////////////////////////////////
+
+echo Selamat datang kak, Siapa nick kaka? #tulisan keluar
+
+read nick #membaca nick yg ditulis
+
+
+echo "   ..  ...  ...  ...  ..      .          ..  ....."
+sleep 0.03
+echo "  ...  .... .... ....     %/\      .. ..  .....  ."
+sleep 0.03
+echo " .  ...  ...  ... ..     %/  &.     ..  ......  .."
+sleep 0.03
+echo " ..  ....  ..  ...     .%/     \        ...  ....."
+sleep 0.03
+echo " ....  ....  ..      .%*/       &.     ...  ..... "
+sleep 0.03
+echo " ......  .... .    .%.*/  .d99b_  \      . ....  ."
+sleep 0.03
+echo "   .......       .%***/ -'       '.&.     ...  ..."
+sleep 0.03
+echo " ..     ..     .%****/  ._.   '~::,  \      . . .."
+sleep 0.03
+echo " .......     .%*****/._'  .' ^':,  :.,&.     . .. "
+sleep 0.03
+echo " ...       .%******/',_-^{  ( )  }    :.\       .."
+sleep 0.03
+echo "   ..     .%******/%^    '.     .'    ;.&.       "
+sleep 0.03
+echo " .     .%********/;        ".,."        ;#. \     "
+sleep 0.03
+echo "      .%********/  ~'.,,.          ,.-'^  &.      "
+sleep 0.03
+echo "    .%*********/         ""-.,.-""~            \    "
+sleep 0.03
+echo "  .%**********/                              &.  "
+sleep 0.03
+echo " %***********/                                 \ "
+sleep 0.03
+echo " ************************************************"
+sleep 0.03
+echo " ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+sleep 0.03
+echo " ================================================"
+sleep 2
+echo " ==          Eye Of All Seeing Project         =="
+sleep 0.7
+echo " ================================================"
+echo Selamat datang $nick ":)" Jones! :v
+echo
+echo "Kaka jones kan? :p"
+echo "jawab?"
+read jawab
+echo
+echo "NGAKAK DASAR JONES !!! :p"
+echo
+echo "Mulai scanning cctv?"
+echo "y/n"
+read confirm
+if [ $confirm = "y" ]; then
+echo "Melakukan scanning"
+i=0
+max=100
+while [ $i -le $max ]; do
+echo -ne "\nScanning percentage : $i% "
+sleep 0.03
+if [ $i -eq 100 ]; then
+echo -ne " [complete!]\n"
+echo "========================================"
+curl -s http://zlucifer.hol.es/Project_eye_of_all_seeing/index.php
+echo " Gunakan tools dengan bijak"
+echo
+echo " Love u always "
+echo " -zLucifer"
+echo "======================================="
+fi
+let i++
+done
+else
+echo "Kesalahan"
+fi
+
